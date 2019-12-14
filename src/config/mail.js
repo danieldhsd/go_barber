be1e2097a12062d269b2f2b04e61eaf06f43869f
Daniel Dutra <danieldhsd@gmail.com>
2019-12-14 10:00:40 -0300

    Configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6d7e5772d95346',
+    pass: '4d2fd51a7e5dd7',
+  },
+  default: {
+    from: 'Team GoBarber <noreply@gobarber.com>',
+  },
+};
